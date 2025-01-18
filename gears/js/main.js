d49201f5d6d4c5775f759b0c5e4ced654f05dced
liledix4 <liledix4@musician.org>
2025-01-18 14:02:33 +0000

    [2025.01.18]

--- gears/js/main.js
+++ gears/js/main.js
@@ -1,10 +1,8 @@
-import { checkIfFilesAreLoaded, fileData, loadSeriesOfFiles } from "../modules/js_load_series_of_files/load_series_of_files.js";
+import { fileData, loadSeriesOfFiles } from "../modules/js_load_series_of_files/load_series_of_files.js";
 import { wrapContent } from "./content.js";
 import { getListOfFeatures } from "./features.js";
 
 
 loadSeriesOfFiles('../../json/main.json', () => {
-    checkIfFilesAreLoaded(() => {
-        wrapContent( getListOfFeatures(fileData) );
-    });
+    wrapContent( getListOfFeatures(fileData) );
 });
